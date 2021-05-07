04032d4835c47f8466ae21e3c5a548f8b367e086
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-07 02:02:37 -0400

    Improve Hero

--- src/sections/HeroSection/index.jsx
+++ src/sections/HeroSection/index.jsx
@@ -2,15 +2,17 @@ import styles from './HeroSection.module.css'
 
 export function HeroSection() {
     return (
-        <section>
+        <section className={styles.HeroSection}>
             <h1 className={styles.HeroSectionTitle}>Desty</h1>
             <p className={styles.HeroSectionSlogan}>Explore the most fascinating places around the world where all of us live.</p>
-            <a href="#places"
-                className={styles.HeroSectionLinkCTA}
-            >Explore</a>
-            <a href="#subscription"
-                className={styles.HeroSectionLink}
-            >Subscribe! (+ stories)</a>
+            <div className={styles.HeroSectionActions}>
+                <a href="#places"
+                    className={styles.HeroSectionLinkCTA}
+                >Explore</a>
+                <a href="#subscription"
+                    className={styles.HeroSectionLink}
+                >Subscribe! (+ stories)</a>
+            </div>
         </section>
     )
 }
