5176ccc0f2f9d32916866a6e65f8c25aef98dd98
Alejandro M. Coca <alxmcr@outlook.com>
2021-05-07 01:15:03 -0400

    Working Header and Footer

--- src/components/cards/PlaceCard/index.jsx
+++ src/components/cards/PlaceCard/index.jsx
@@ -3,12 +3,14 @@ import styles from './PlaceCard.module.css'
 export function PlaceCard({ place }) {
     const { id, image, name, description } = place;
     return (
-        <div id={id}>
-            <img src={image} />
+        <div id={id} className={styles.PlaceCard}>
+            <img src={image} className={styles.PlaceImage} />
             <div>
-                <h3>{name}</h3>
-                <p>{description}</p>
-                <a href="#newsletter">More stories 🤩</a>
+                <h3 className={styles.PlaceName}>{name}</h3>
+                <p className={styles.PlaceDescription}>{description}</p>
+                <a href="#newsletter"
+                    className={styles.PlaceLink}
+                >More stories 🤩</a>
             </div>
         </div>
     )
--- src/components/common/FooterApp/index.jsx
+++ src/components/common/FooterApp/index.jsx
@@ -1,3 +1,4 @@
+import { faFacebook, faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './FooterApp.module.css'
@@ -9,26 +10,31 @@ export function FooterApp() {
                 <li className={styles.FooterAppItem}>
                     <a href="https://github.com/alxmcr/desty-app"
                         className={styles.FooterAppLink}>
+                        <FontAwesomeIcon icon={faGithub} />
                     </a>
                 </li>
                 <li className={styles.FooterAppItem}>
                     <a href="https://instagram.com/desty-app"
                         className={styles.FooterAppLink}>
+                        <FontAwesomeIcon icon={faInstagram} />
                     </a>
                 </li>
                 <li className={styles.FooterAppItem}>
                     <a href="https://facebook.com/desty-app"
                         className={styles.FooterAppLink}>
+                        <FontAwesomeIcon icon={faFacebook} />
                     </a>
                 </li>
                 <li className={styles.FooterAppItem}>
                     <a href="https://twitter.com/desty-app"
                         className={styles.FooterAppLink}>
+                        <FontAwesomeIcon icon={faTwitter} />
                     </a>
                 </li>
                 <li className={styles.FooterAppItem}>
                     <a href="https://linkedin.com/desty-app"
                         className={styles.FooterAppLink}>
+                        <FontAwesomeIcon icon={faLinkedin} />
                     </a>
                 </li>
             </ul>
